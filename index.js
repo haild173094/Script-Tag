bcbc098a7f367efc7d5915f4a1f367680b3bb251
YourName <you@mail.com>
2022-04-13 15:35:15 +0700

    update file css, js

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
-app.listen(process.env.PORT || 7000);
+app.listen(process.env.PORT || 6999);
 
-console.log('Server is running on port 7000');
+console.log('Server is running on port 6999');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+let jsFile = null;
+let cssFile = null;
 
 const readFilePromise = (filePath) => {
     return new Promise((resolve, reject) => {
@@ -22,17 +25,38 @@ const readFilePromise = (filePath) => {
     });
 }
 
+const readDirPromise = (dirPath) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dirPath, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(files);
+            }
+        });
+    });
+}
+
 app.get('/test.js', async (req, res) => {
-    let file = await readFilePromise(__dirname + '/sdk/dist/assets/index.fd68666d.js');
+    let files = await readDirPromise(__dirname + '/sdk/dist/assets');
+    files.forEach(file => {
+        if (file.include('js')) {
+            jsFile = file;
+        }
+        else if (file.include('css')) {
+            cssFile = file;
+        }
+    });
+    let file = await readFilePromise(__dirname + `/sdk/dist/assets/${jsFile}`);
     file += `
                     const styleSheetLink  = document.createElement('link');
                     styleSheetLink.rel = 'stylesheet';
-                    styleSheetLink.href = 'https://aqueous-anchorage-20803.herokuapp.com/index.e46485b0.css';
+                    styleSheetLink.href = 'https://aqueous-anchorage-20803.herokuapp.com/index.css';
                     document.head.appendChild(styleSheetLink);
             `;
     res.send(file);
 });
 
-app.get('/index.e46485b0.css', (req, res) => {
-    res.sendFile(__dirname + '/sdk/dist/assets/index.e46485b0.css');
+app.get('/index.css', (req, res) => {
+    res.sendFile(__dirname + `/sdk/dist/assets/${cssFile}`);
 })
