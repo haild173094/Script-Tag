3de19706e5a0c5cc778fe9e948a904a05b124f0e
YourName <you@mail.com>
2022-04-13 15:13:29 +0700

    Append css

--- index.js
+++ index.js
@@ -9,8 +9,25 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+const readFilePromise = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
 app.get('/test.js', (req, res) => {
-    res.sendFile(__dirname + '/sdk/dist/assets/index.fd68666d.js');
+    const file = await readFilePromise(__dirname + '/sdk/dist/assets/index.fd68666d.js');
+    file.append(`const styleSheetLink  = document.createElement('link');
+                    styleSheetLink.rel = 'stylesheet';
+                    styleSheetLink.href = 'https://aqueous-anchorage-20803.herokuapp.com/index.e46485b0.css';
+                    document.head.appendChild(styleSheetLink);`);
+    res.send(file);
 });
 
 app.get('/index.e46485b0.css', (req, res) => {
