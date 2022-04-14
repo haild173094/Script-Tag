8b2e9c6763ba39131399b0e28dd94d8c3cce47dd
YourName <you@mail.com>
2022-04-14 09:32:39 +0700

    delete pv css

--- sdk/src/main.js
+++ sdk/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './bootstrap'
 import AppConfig from './configs/app';
 import PolarisVue from '@qikify/polaris-vue'
-import '@qikify/polaris-vue/dist/style.css'
+// import '@qikify/polaris-vue/dist/style.css'
 
 
 createApp(App).use(PolarisVue).mount(`#${AppConfig.ELEMENT_ID}`)
