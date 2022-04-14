6b03eb44151f66c1ebfab09dd55514d8abd04398
YourName <you@mail.com>
2022-04-14 10:55:41 +0700

    test popover

--- sdk/src/ScriptTag.vue
+++ sdk/src/ScriptTag.vue
@@ -15,10 +15,10 @@
                 class="btn btn-primary" 
                 data-bs-target="#collapseTarget" 
                 data-bs-toggle="collapse">
-                Bootstrap collapse
+                    Bootstrap collapse
                 </button>
                 <div class="collapse py-2" id="collapseTarget">
-                This is the toggle-able content!
+                     This is the toggle-able content!
                 </div>
             
     
--- sdk/src/main.js
+++ sdk/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import AppConfig from './configs/app'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import "./bootstrapSetup"
 
 // import PolarisVue from '@qikify/polaris-vue'
 // import '@qikify/polaris-vue/dist/style.css'
